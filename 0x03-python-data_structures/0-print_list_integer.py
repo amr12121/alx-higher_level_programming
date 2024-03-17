b4b0f45cc38210fe81c34d576d3b8bd8cdb73ade
amr12121 <eltohmyamr@gmail.com>
2024-03-17 16:32:09 -0700

    a

---
+++ 0x03-python-data_structures/0-print_list_integer.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+
+if _name__ == "__main__":
+    def print_list_integer(my_list=[]):
+        for i in my_list[]:
+             print('{:d}'.format(i))
+
---
+++ 0x03-python-data_structures/README.md
@@ -0,0 +1,1 @@
+tasks to project date structures
