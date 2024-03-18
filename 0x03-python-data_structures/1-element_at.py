8eadd229ea4e0adb422dd01e5ae4f6a59d5be2ac
amr12121 <eltohmyamr@gmail.com>
2024-03-18 14:50:25 -0700

    a

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -1,9 +1,7 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
     def element_at(my_list, idx):
-        if idx < 0:
+        if idx < 0 or idx > len(my_lis) - 1:
             return 'None'
-        elif idx is not in range(my_list):
-            return' None'
         else:
             print(my_list[idx])
