39891184d9b151d48bc30f6c1c5db08f8b86e83d
amr12121 <eltohmyamr@gmail.com>
2024-03-18 14:39:28 -0700

    a

---
+++ 0x03-python-data_structures/1-element_at.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    def element_at(my_list, idx):
+        if idx < 0:
+            return None
+        elif idx is not in range(my_list):
+            return None
